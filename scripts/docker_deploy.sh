d30ad7945fba7d6fe0fc7056eeb3a008e7ebcf91
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2026-03-24 00:59:06 +0800

    update version to 0.42.2

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -7,5 +7,5 @@ GO_PTTBBS_VERSION=`grep "go-pttbbs v" go.mod|awk '{print $2}'`
 echo "GO_PTTBBS_VERSION: ${GO_PTTBBS_VERSION}"
 
 docker login
-docker buildx build --platform linux/amd64 -t pttofficialapps/${project}:${branch} -f docker/Dockerfile --build-arg GO_PTTBBS_VERSION=${GO_PTTBBS_VERSION} --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t pttofficialapps/${project}:${branch} -f docker/Dockerfile --build-arg GO_PTTBBS_VERSION=${GO_PTTBBS_VERSION} --push .
 docker buildx prune -f
